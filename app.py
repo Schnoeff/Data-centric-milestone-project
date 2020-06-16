b21703e28844ab19dd7879126a725ebd811fb881
Schnoeff <02bburrell@live.co.uk>
2020-06-16 11:28:48 +0000

    MongoDB connection

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipes'
+app.config["MONGO_URI"] = 'mongodb+srv://Schnoeff:Brooker3798@recipies-ewzrm.mongodb.net/recipes?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
 def hello():
     return 'Hello World ...again'
 
